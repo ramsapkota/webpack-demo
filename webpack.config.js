60b33ccf2d45d6460dbb6d1c4dffee6818a91673
ram.sapkota <ram.sapkota@sourcecode.com.np>
2019-08-06 12:59:34 +0545

    Working with css-loader and style-loader to bundle css and use them in dom

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
 module.exports = {
   mode: "development", //Explicitly set webpack to bundle without minification
-  devtool: "none", //Explicitly tell webpack to export same code that is in them codefiles
+  //devtool: "none", //Explicitly tell webpack to export same code that is in them codefiles
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   }
 };
