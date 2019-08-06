33add55719386d1b07c2e8a408103bb46739f64c
ram.sapkota <ram.sapkota@sourcecode.com.np>
2019-08-06 14:26:51 +0545

    Added HtmlWebPackPlugin to dynamically inject script in our custom tempalte

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 module.exports = {
   mode: "development", //Explicitly set webpack to bundle without minification
   //devtool: "none", //Explicitly tell webpack to export same code that is in them codefiles
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main.[contentHash].js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
-          "style-loader", // Third injects css-javascript into dom
+          "style-loader", // Th ird injects css-javascript into dom
           "css-loader", // Second converts css into commonjs
           "sass-loader" //First converts sass into css
         ]
